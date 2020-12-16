46591eedfb888014b1710abdda8a782729c98e98
Suyatna <zuyatna@gmail.com>
2020-12-17 00:48:28 +0700

    add hero and attack animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 namespace Player
@@ -17,10 +16,17 @@ namespace Player
         private static readonly int Horizontal = Animator.StringToHash("Horizontal");
         private static readonly int Vertical = Animator.StringToHash("Vertical");
         private static readonly int Speed = Animator.StringToHash("Speed");
-        private static readonly int Melee = Animator.StringToHash("Attack_Melee");
 
-        private bool _isAttackMelee;
+        private static readonly int Combo1 = Animator.StringToHash("Attack1");
+        private static readonly int Combo2 = Animator.StringToHash("Attack2");
+        private static readonly int Combo3 = Animator.StringToHash("Attack3");
         
+        private bool _isAttack;
+        private bool _isFlipX;
+
+        private int[] _comboAttack = new int[3];
+        private int _attackCount;
+
         // Update is called once per frame
         void Update()
         {        
@@ -31,31 +37,85 @@ namespace Player
             animator.SetFloat(Vertical, _movement.y);
             animator.SetFloat(Speed, _movement.sqrMagnitude);
 
-            if (Input.GetMouseButtonDown(0))
+            if (_movement.x < 0) gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            if (_movement.x > 0) gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+            if (Input.GetMouseButtonDown(0) && !_isAttack)
             {
-                StartCoroutine(AttackMelee());
+                _attackCount++;
+                _attackCount = _attackCount > 3 ? _attackCount = 1 : _attackCount++;
+
+                switch (_attackCount)
+                {
+                    case 1:
+                        StartCoroutine(Attack1());
+                        break;
+                    case 2:
+                        StartCoroutine(Attack2());
+                        break;
+                    case 3:
+                        StartCoroutine(Attack3());
+                        break;
+                    default:
+                        Debug.Log("Attack Count Out Of Range() ");
+                        break;
+                }
+
+                Debug.Log("Attack Count : " +_attackCount);
             }
         }
 
         private void FixedUpdate()
         {
-            if (!_isAttackMelee)
+            if (!_isAttack)
             {
                 rigidbody2d.MovePosition(rigidbody2d.position + _movement * (moveSpeed * Time.fixedDeltaTime));
             }
         }
 
-        private IEnumerator AttackMelee()
+        private IEnumerator Attack1()
         {
-            animator.SetBool(Melee, true);
-            _isAttackMelee = true;
-            Debug.Log("Attack!!");
-
-            yield return new WaitForSeconds(0.3f);
+            _isAttack = true;
             
-            animator.SetBool(Melee, false);
-            _isAttackMelee = false;
-            Debug.Log("Stop Attack!!");
+            animator.SetBool(Combo1, _isAttack);
+            
+            Debug.Log("Attack 1");
+
+            yield return new WaitForSeconds(0.2f);
+            
+            _isAttack = false;
+            
+            animator.SetBool(Combo1, _isAttack);
+        }
+        
+        private IEnumerator Attack2()
+        {
+            _isAttack = true;
+            
+            animator.SetBool(Combo2, _isAttack);
+            
+            Debug.Log("Attack 2");
+
+            yield return new WaitForSeconds(0.2f);
+            
+            _isAttack = false;
+            
+            animator.SetBool(Combo2, _isAttack);
+        }
+        
+        private IEnumerator Attack3()
+        {
+            _isAttack = true;
+            
+            animator.SetBool(Combo3, _isAttack);
+            
+            Debug.Log("Attack 3");
+
+            yield return new WaitForSeconds(0.2f);
+            
+            _isAttack = false;
+            
+            animator.SetBool(Combo3, _isAttack);
         }
     }
 }
